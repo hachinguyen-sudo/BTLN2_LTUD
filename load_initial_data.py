520cd691d3064c997d417a1fb7a66a860d51db5f
P-code2025 <phu.b24cc225@stu.ptit.edu.vn>
2026-06-08 11:32:23 +0700

    Fix auto load data condition

--- load_initial_data.py
+++ load_initial_data.py
@@ -5,10 +5,10 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CleanSpace.settings')
 django.setup()
 
 from django.core.management import call_command
-from django.contrib.auth.models import User
+from apps.services.models import Service
 
-if not User.objects.exists():
-    print("Database is empty. Loading data.json...")
+if not Service.objects.exists():
+    print("Database is missing services. Loading data.json...")
     call_command('loaddata', 'data.json')
 else:
     print("Database already has data. Skipping loaddata.")
